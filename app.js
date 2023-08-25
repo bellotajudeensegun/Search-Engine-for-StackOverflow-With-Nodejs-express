a55a6bbe0421904df838b207346e59f26741dd4e
SEGUN BELLO <123709089+bellotajudeensegun@users.noreply.github.com>
2023-08-25 03:37:08 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const axios = require('axios');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -8,6 +9,27 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render('search');
 });
+
+
+
+app.get('/search', async (req, res) => {
+  const query = req.query.query;
+  // Use the Stack Overflow API to search for questions based on the query
+  const apiUrl = `STACK_OVERFLOW_API_URL_HERE`;
+  
+  try {
+    const response = await axios.get(apiUrl);
+    const results = response.data.items.map(item => ({
+      title: item.title,
+      link: item.link
+    }));
+    res.render('search', { results });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching search results');
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
